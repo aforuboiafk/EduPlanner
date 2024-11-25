5cfb5e97a4c765a7d8463ff6ab1a60223e0e6460
Yama <KuChannel@hotmail.com>
2024-11-25 01:05:25 -0300

    Yama: se crea el calendario | Frano: crea los modelos

--- EduPlanner/core/models.py
+++ EduPlanner/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=150)
+    descripcion = models.TextField()
+    fecha_inicio = models.DateTimeField()
+    fecha_fin = models.DateTimeField()
+    tipo = models.CharField(max_length=50)
+    aprobado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
+    
+class Feriado(models.Model):
+    nombre = models.CharField(max_length=150)
+    fecha = models.DateField()
+    tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.fecha})"
+    
--- EduPlanner/core/views.py
+++ EduPlanner/core/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+import calendar
+from datetime import datetime
 
 # Create your views here.
+
+from django.shortcuts import render
+
+
+
+
+def home(request):
+    # Obtén el mes y año de los parámetros GET o usa el actual
+    year = int(request.GET.get('year', datetime.now().year))
+    month = int(request.GET.get('month', datetime.now().month))
+    
+    # Genera la información del calendario
+    cal = calendar.Calendar()
+    days = cal.monthdayscalendar(year, month)  # Días organizados por semanas
+    month_name = calendar.month_name[month]
+    print(year, month,days)
+    
+    data = {
+        'days': days,
+        'month': month,
+        'year': year,
+        'month_name': month_name,
+        'prev_month': month - 1 if month > 1 else 12,
+        'next_month': month + 1 if month < 12 else 1,
+        'prev_year': year - 1 if month == 1 else year,
+        'next_year': year + 1 if month == 12 else year,
+    }
+    return render(request, 'core\home.html', data)
